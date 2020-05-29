f772347e2cbf19b72c9a628621c11c5ff8099e80
Emiliana Vargas <emilianavargasverdejo@gmail.com>
2020-05-29 02:08:23 -0300

    cambio a .ejs vistas - cambio de todas los metodos del controlador

--- site/controllers/indexController.js
+++ site/controllers/indexController.js
@@ -1,66 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 
-/*function readHTML(index){
-    let archivoHTML = fs.readFileSync(path.join(__dirname,'/../views/' + index),'utf-8')
-    return archivoHTML;
-}
-
-/ ************ Function to Read an HTML File ************
+/************ Function to Read an HTML File ************ YA NO SE UTILIZA
 */
-function readHTML (fileName) {
-	let htmlFile = fs.readFileSync(path.join(__dirname, '/../views/'+fileName+'.html'), 'utf-8');
-	return htmlFile;
-}
+// function readHTML (fileName) {
+// 	let htmlFile = fs.readFileSync(path.join(__dirname, '/../views/'+fileName+'.html'), 'utf-8');
+// 	return htmlFile;
+// }
 
 
 let indexController = {
     'index': function(req,res){
-            let indexHTML = readHTML('index');
-            res.send(indexHTML);
+            res.render('index');
     },
     'register': function(req,res){
-        let registerHTML = readHTML('register');
-            res.send(registerHTML);
+        res.render('register');
     },
     'login': function(req,res){
-        let loginHTML = readHTML('login');
+        res.render('login');
             res.send(loginHTML);
     },
     'productDetail': function(req,res){
-        let productDetailHTML = readHTML('productDetail');
-            res.send(productDetailHTML);
+        res.render('productDetail');
     },
     'products': function(req,res){
-        let productsHTML = readHTML('products');
-            res.send(productsHTML);
+        res.render('products');
     },
     'productCart': function(req,res){
-        let productCartHTML = readHTML('productCart');
-            res.send(productCartHTML);
+        res.render('productCart');
     },
     'productAdd': function(req,res){
-        let productAddHTML = readHTML('productAdd');
-            res.send(productAddHTML);
+        res.render('productAdd');
     },
     'productCartPayment': function(req,res){
-        let productCartPaymentHTML = readHTML('productCartPayment');
-            res.send(productCartPaymentHTML);
+        res.render('productCartPayment');
     }
 }
 
 module.exports = indexController;
 
 
-
-/*ver un resumen de la oferta - home
-/producto:toda la información relevante del producto y poder agregar fácilmente el producto a mi carrito para poder
-comprarlo
-/carrito:detalle de mi carrito de compras en cualquier momento para finalizar la compra y poder modificar productos del carrito para poder eliminarlos
-de la compra o modificar la cantidad.
-/registro: registrarme para acceder a mi historial de compras y facilitar futuras compras.
-/Página del backend de administración donde el personal del sitio carga nuevos
-productos y gestiona los existentes:facilidad, saber si un campo es obligatorio para acelerar la
-carga,  que el formulario me marque los errores para evitar cargar productos incompletos o erróneos.(No será necesario implementar ninguna funcionalidad más que los enlaces entre las diferentes
-páginas. Para consignas como la validación de formularios deberán mostrar cómo se verán los errores aún si el formulario no esté funcional.)
-*/
