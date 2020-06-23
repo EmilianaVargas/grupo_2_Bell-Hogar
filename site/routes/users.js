ae8a76745b7ee427e21d9d66fc92928211c7b4f7
sora9806 <sorany.jimenez@gmail.com>
2020-06-22 23:11:27 -0300

    adicionando ruta de usuarios

--- site/routes/users.js
+++ site/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+let usersController = require('../controllers/usersController');
+const middUploadFile = require('../middlewares/middUploadFile');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+// Ruta de listado de usuarios
+router.get('/', usersController.users);
+
+// Ruta de creación de usuarios
+router.get('/create', usersController.createUsers);
+router.post('/create', middUploadFile.uploadFile, usersController.postUser);
 
 module.exports = router;
