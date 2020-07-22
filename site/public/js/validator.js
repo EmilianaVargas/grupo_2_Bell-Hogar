f21ce8231cc727a530805bf34d061cb240ceca6b
Emiliana Vargas <emilianavargasverdejo@gmail.com>
2020-07-22 02:29:59 -0300

    validación frontend CRUD Productos
    falta unificar estilo para el mensaje de error en todas las vistas

---
+++ site/public/js/validator.js
@@ -0,0 +1,124 @@
+window.addEventListener("load", function(){
+    let formulario = document.querySelector("form.productAdd")
+    formulario.addEventListener("submit", function(evento){
+        let errores = [];
+
+        let campoNombre = document.querySelector("input#nombre");
+            if(campoNombre.value.length<5){
+                evento.preventDefault();
+            errores.push("El campo nombre debe tener al menos 5 caracteres");
+            alert("El campo nombre debe tener al menos 5 caracteres");
+            }
+        let campoDescripcion = document.querySelector("input#descripcion");
+        if(campoDescripcion.value.length<20){
+            evento.preventDefault();
+            errores.push("El campo descripción debe tener al menos 20 caracteres");
+            alert("El campo descripción debe tener al menos 20 caracteres");
+            }
+        let campoCategoria = document.querySelector("input#categoria");
+        if(campoCategoria.value == ""){
+            evento.preventDefault();
+            errores.push("El campo categoria está vacío");
+            alert("El campo categoria no debe estar vacío");
+            }
+        let campoSubcategoria = document.querySelector("input#subcategoria");
+        if(campoSubcategoria.value == ""){
+            evento.preventDefault();
+            errores.push("El campo subcategoria está vacío");
+            alert("El campo subcategoria no debe estar vacío");
+            }
+        let campoMarca = document.querySelector("input#marca");
+        if(campoMarca.value == ""){
+            evento.preventDefault();
+            errores.push("El campo marca está vacío");
+            alert("El campo marca no debe estar vacío");
+            }
+        let campoPrecio = document.querySelector("input#precio");
+        if(campoPrecio.value < 0){
+            evento.preventDefault();
+            errores.push("El campo precio debe ser un número positivo");
+            alert("El campo precio debe ser un número positivo");
+            }
+        let campoImage1 = document.querySelector("input#image1");
+            if(campoImage1.value == ""){
+                evento.preventDefault();
+                errores.push("El campo imagen está vacío");
+                alert("El campo imagen no debe estar vacío");
+            }
+        let campoStock = document.querySelector("input#stock");
+            if(campoStock.value < 0){
+                evento.preventDefault();
+                errores.push("El campo Stock debe ser un número positivo");
+                alert("El campo Stock debe ser un número positivo");
+            }
+
+        if(errores.length > 0){
+            let ulErrores = document.querySelector("div.errores ul");
+                for (let i = 0; i < errores.length; i++) {
+                ulErrores.innerHTML += "<li>" +errores[i]+"</li>";
+            }
+        }
+    });
+});
+window.addEventListener("load", function(){
+    let formulario = document.querySelector("form.editProduct")
+    formulario.addEventListener("submit", function(evento){
+        let errores = [];
+
+        let campoNombre = document.querySelector("input#nombre");
+            if(campoNombre.value.length<5){
+                evento.preventDefault();
+            errores.push("El campo nombre tiene menos de 5 caracteres");
+            alert("El campo nombre debe tener al menos 5 caracteres");
+            }
+        let campoDescripcion = document.querySelector("input#descripcion");
+        if(campoDescripcion.value.length<20){
+            evento.preventDefault();
+            errores.push("El campo descripción debe tener al menos 20 caracteres");
+            alert("El campo descripción debe tener al menos 20 caracteres");
+            }
+        let campoCategoria = document.querySelector("input#categoria");
+        if(campoCategoria.value == ""){
+            evento.preventDefault();
+            errores.push("El campo categoria está vacío");
+            alert("El campo categoria no debe estar vacío");
+            }
+        let campoSubcategoria = document.querySelector("input#subcategoria");
+        if(campoSubcategoria.value == ""){
+            evento.preventDefault();
+            errores.push("El campo subcategoria está vacío");
+            alert("El campo subcategoria no debe estar vacío");
+            }
+        let campoMarca = document.querySelector("input#marca");
+        if(campoMarca.value == ""){
+            evento.preventDefault();
+            errores.push("El campo marca está vacío");
+            alert("El campo marca no debe estar vacío");
+            }
+        let campoPrecio = document.querySelector("input#precio");
+        if(campoPrecio.value < 0){
+            evento.preventDefault();
+            errores.push("El campo precio debe ser un número positivo");
+            alert("El campo precio debe ser un número positivo");
+            }
+        let campoImage1 = document.querySelector("input#image1");
+            if(campoImage1.value == ""){
+                evento.preventDefault();
+                errores.push("El campo imagen está vacío");
+                alert("El campo imagen no debe estar vacío");
+            }
+        let campoStock = document.querySelector("input#stock");
+            if(campoStock.value < 0){
+                evento.preventDefault();
+                errores.push("El campo Stock debe ser un número positivo");
+                alert("El campo Stock debe ser un número positivo");
+            }
+
+        if(errores.length > 0){
+            let ulErrores = document.querySelector("div.errores ul");
+                for (let i = 0; i < errores.length; i++) {
+                ulErrores.innerHTML += "<li>" +errores[i]+"</li>";
+            }
+        }
+    });
+});
